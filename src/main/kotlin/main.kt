2b8c8c2768a1404c23eb6006a7e1680f1b815fc8
Mark Schaefer <markscha@hellmann.com>
2023-07-02 21:43:48 +0200

    Tokens and error reporting

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,49 @@
-fun main() {
+import java.io.File
+import java.util.*
+import kotlin.system.exitProcess
 
+fun main(args: Array<String>) {
+    if (args.size > 1) {
+        println("""Usage klox [script]""")
+        exitProcess(64)
+    } else if (args.size == 1) {
+        runFile(args[0])
+    } else {
+        runPrompt()
+    }
+
+    if (hadError) exitProcess(65)
+}
+
+fun runFile(path: String) {
+    run(File(path).readText())
+}
+
+fun runPrompt() {
+    while (true) {
+        print("> ")
+        val line = readln()
+        if (line.isEmpty()) break
+        run(line)
+        hadError = false
+    }
+}
+
+fun run(source: String) {
+
+    val scanner = Scanner(source)
+    val tokens = scanner.tokens()
+
+    tokens.forEach { println(it) }
+}
+
+fun error(line: Int, message: String) {
+    report(line, "", message)
+}
+
+var hadError = false
+
+fun report(line: Int, where: String, message: String) {
+    System.err.println("[line $line] Error$where: $message")
+    hadError = true
 }
